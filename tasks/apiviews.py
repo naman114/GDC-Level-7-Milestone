86029d0d3f010c17ded98a3385f90d66719be77c
Naman Gogia <namangogia2001@gmail.com>
2022-02-25 18:34:48 +0530

    Initial commit

---
+++ tasks/apiviews.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import User
+from django_filters.rest_framework import (
+    CharFilter,
+    ChoiceFilter,
+    DjangoFilterBackend,
+    FilterSet,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
+from tasks.models import Task
+
+STATUS_CHOICES = (
+    ("PENDING", "PENDING"),
+    ("IN_PROGRESS", "IN_PROGRESS"),
+    ("COMPLETED", "COMPLETED"),
+    ("CANCELLED", "CANCELLED"),
+)
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username")
+
+
+class TaskFilter(FilterSet):
+    title = CharFilter(lookup_expr="icontains")
+    status = ChoiceFilter(choices=STATUS_CHOICES)
+
+
+class TaskSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ["title", "description", "completed", "user"]
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    permission_classes = (IsAuthenticated,)
+
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TaskFilter
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user, deleted=False)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskListAPI(APIView):
+    def get(self, request):
+        tasks = Task.objects.filter(deleted=False)
+        data = TaskSerializer(tasks, many=True).data
+        return Response({"tasks": data})
